ce9b79e10e806b2fc0a807099a43ebee9603bf29
Nathanael Ferry P <ntnferry1505@gmail.com>
2023-10-26 22:09:32 +0700

    Create Template

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('title', 'Home')
+
+@section('content')
+    <div id="title">
+        <div class="card card-body">
+            <h3>UTS Pemrograman Berbasis Objek Lanjut</h3>
+            <p>
+                <b>Nama</b> : Nathanael Ferry Pratama<br>
+                <b>NPM</b> : 2021130019
+            </p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') - {{ config('app.name', 'T1-PBWL') }}</title>
+    @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+        <div class="container container-fluid">
+            <a class="navbar-brand" href="#">UTS - PBWL</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Buku</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="container my-4">
+        @yield('content')
+    </main>
+
+</body>
+
+</html>
