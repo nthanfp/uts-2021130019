cd48f820c102fb96353048d21b5117f1fcd4c808
Nathanael Ferry P <ntnferry1505@gmail.com>
2023-10-26 22:43:06 +0700

    Update Web Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
+Route::resource('transaction', TransactionController::class);
